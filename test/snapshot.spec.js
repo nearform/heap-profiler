d370f00d681a85148defc20c27d22e359077d4e8
Shogun <shogun@cowtech.it>
2020-04-01 15:18:24 +0200

    chore: Uncomment mistakenly commented tests.

--- test/snapshot.spec.js
+++ test/snapshot.spec.js
@@ -7,17 +7,17 @@ const { file: tmpFile } = require('tmp-promise')
 const generateHeapSnapshot = require('../src/snapshot')
 const validate = require('./fixtures/snapshotSchema')
 
-// t.test('it correctly generates a snapshot using promises', async t => {
-//   const { path: destination, cleanup } = await tmpFile()
+t.test('it correctly generates a snapshot using promises', async t => {
+  const { path: destination, cleanup } = await tmpFile()
 
-//   await generateHeapSnapshot({ destination })
+  await generateHeapSnapshot({ destination })
 
-//   const generated = JSON.parse(readFileSync(destination, 'utf-8'))
+  const generated = JSON.parse(readFileSync(destination, 'utf-8'))
 
-//   t.true(validate(generated))
+  t.true(validate(generated))
 
-//   cleanup()
-// })
+  cleanup()
+})
 
 t.test('it correctly generates a snapshot using callbacks', t => {
   tmpFile().then(({ path: destination, cleanup }) => {
@@ -31,18 +31,18 @@ t.test('it correctly generates a snapshot using callbacks', t => {
   })
 })
 
-// t.test('it handles file saving errors using promises', async t => {
-//   await t.rejects(() => generateHeapSnapshot({ destination: '/this/doesnt/exists' }), {
-//     message: "ENOENT: no such file or directory, open '/this/doesnt/exists'"
-//   })
-// })
+t.test('it handles file saving errors using promises', async t => {
+  await t.rejects(() => generateHeapSnapshot({ destination: '/this/doesnt/exists' }), {
+    message: "ENOENT: no such file or directory, open '/this/doesnt/exists'"
+  })
+})
 
-// t.test('it handles file saving errors using callbacks', t => {
-//   generateHeapSnapshot({ destination: '/this/doesnt/exists' }, err => {
-//     t.equal(err.message, "ENOENT: no such file or directory, open '/this/doesnt/exists'")
-//     t.end()
-//   })
-// })
+t.test('it handles file saving errors using callbacks', t => {
+  generateHeapSnapshot({ destination: '/this/doesnt/exists' }, err => {
+    t.equal(err.message, "ENOENT: no such file or directory, open '/this/doesnt/exists'")
+    t.end()
+  })
+})
 
 t.test('it validates the destination option', t => {
   t.throws(() => generateHeapSnapshot({ destination: 1 }), 'The destination option must be a non empty string')
